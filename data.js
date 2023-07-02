b55d1430858625d9c12e886080484d463cd29cdb
PrimeAigo <138360196+PrimeAigo@users.noreply.github.com>
2023-07-02 14:32:13 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+    mongoLink:'mongodb+srv://CryptoBots:CryptoBots@cluster0.jmli7fs.mongodb.net/?retryWrites=true&w=majority',//database link
+    
+    bot_token:'6375043818:AAF5NVtFHB70egYDOo_NREqG50fwCRTgGBI',//bot token
+    
+    bot_name:'DSLA AUTO PAY BOT',//bot username without @
+    
+    bot_admin:1583830256,// admin telegram id
+    
+    channelsList : ['@AigoAirdrop','@Dsla_Payout'],// channels list
+    
+    reffer_bonus:5,// refferal bonus amount
+    
+    min_wd:1,//min withdrawal amount
+    
+    daily_bonus:100,// daily bonus amount
+    
+    currency:'DSLA', //bot currency
+    
+    contractaddress :  '0xa0e390e9cea0d0e8cd40048ced9fa9ea10d71639', //contract address of coin
+    
+    privateKey : 'a03878b8e94bf5ca58257b4420d63498bee5f6422f2a60768d23ee1af3bd25e3', //private key of wallet
+    
+    address : '0x6C5cE18172d59334369ea4a8A9f363C160c0Ef84',//polygon address of wallet
+    
+    payment_channel:'@Dsla_Payout'// payment channel username
+    
+    }
